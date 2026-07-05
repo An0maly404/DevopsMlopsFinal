c9fb76903e524ba2193098479d594577a3f5e298
An0maly404 <68429900+An0maly404@users.noreply.github.com>
2026-07-05 14:13:28 +0200

    Docker compose working successfully !

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,11 @@ from pydantic import BaseModel
 from contextlib import asynccontextmanager
 from registry import get_production_model
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
 ml_model = {}
 
 @asynccontextmanager
@@ -24,11 +29,12 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 @app.get("/health")
